e9037cbc582c1a7b9678ad28689ea7d9e30762b8
Pavel Pscheidl <pavel.junior@pscheidl.cz>
2017-04-02 11:58:37 +0200

    Sample endpoint

---
+++ src/main/java/cz/pscheidl/javaee/showcase/SwarmApi.java
@@ -0,0 +1,11 @@
+package cz.pscheidl.javaee.showcase;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Pavel Pscheidl <pavel.junior@pscheidl.cz>
+ */
+@ApplicationPath("")
+public class SwarmApi extends Application {
+}
